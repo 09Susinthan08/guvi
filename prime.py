8c230bf8f833ad6db9b5f580e5f0e2534c31993e
inbasusi8 <40240090+09Susinthan08@users.noreply.github.com>
2019-01-08 12:01:37 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
-number = int(input())
-if number > 1:
-    for i in range(2, number):
-        if (number % i) == 0:
+a=int(input())
+if a> 1:
+    for i in range(2, a):
+        if (a % i) == 0:
             print("no")
             break
     else:
