1a1f04bdb6985a45871aae552ff809d76b52f12f
inbasusi8 <40240090+09Susinthan08@users.noreply.github.com>
2019-01-08 15:47:19 +0530

    Update beteven.py

--- beteven.py
+++ beteven.py
@@ -1,6 +1,10 @@
-l,u=map(int,input().split(" "))
-for i in range(l+1,u):
-    if(i%2==0):
-        print(i,end=" ")
-    else:
-        ()
+a,b=map(int,input().split(" "))
+li=[]
+for i in range (a+1,b):
+	if(i%2==0):
+		li.append(i)
+for i in range (len(li)):
+	if(i==(len(li)-1)):
+		print(li[i],end="")
+	else:
+		print(li[i],end=" ")
