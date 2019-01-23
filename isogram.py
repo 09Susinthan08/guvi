dd2b73bea98ae8f94092126acc552f19533cfe31
inbasusi8 <40240090+09Susinthan08@users.noreply.github.com>
2019-01-23 15:39:28 +0530

    Update isogram.py

--- isogram.py
+++ isogram.py
@@ -2,9 +2,9 @@ def is_isogram(word):
     if type(word) == str:
       for i in word:
         if word.count(i) >1 or word == "":
-          return print("NO")
+          return print("No")
         else:
-          return print("YES")
+          return print("Yes")
     else:
       raise TypeError ("'{}' should be a string" .format(word))
 s=input()
